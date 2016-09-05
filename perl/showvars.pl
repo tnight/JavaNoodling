bc506ed17d349c4b3fc0c8e8a7c1c3318b444030
Terry Nightingale <tnight@pobox.com>
2016-09-05 15:17:27 -0700

    Added another Perl script I found elsewhere.

--- perl/showvars.pl
+++ perl/showvars.pl
@@ -1,36 +1,57 @@
-#!/usr/local/bin/perl
+#! /usr/bin/perl -w
+#===========================================================================
+#
+# Program: showvars.pl
+#
+# Copywright 2000  Ironworx LLC <http://www.ironworx.com>
+#
+# Purpose: Show the environment, CGI vars, and current directory.
+#
+# Created: 02/01/2000 by Terry Nightingale <tnight@ironworx.com>
+#
+# Changed: 99/99/9999 by ... to ...
+#
+#---------------------------------------------------------------------------
 
-my ($key, $value);
+use strict;
 
-print "Content-Type: text/html\n\n";
-
-print "<html><head><title>Test</title></head><body bgcolor='ffffff'><pre>\n";
-
-print "Environment Dump:\n\n";
-
-foreach $key (sort(keys(%ENV))) {
-    print "[$key] = [$ENV{$key}]\n"
-}
-
-print "\n\n";
-
-print "CGI variable dump:\n\n";
+# Necessary for DGC Server
+use lib "/u/web/dgcweb/perl-lib";
 
 use CGI;
+use Cwd;
 
-my $query = new CGI;
+# Scalar to hold our text output.
+my $output = '';
 
-foreach $key (sort($query->param())) {
-    $value = $query->param($key);
-    print "[$key] = [$value]\n";
-}
+# Hash iterator variable.
+my $key;
 
-print "Cookie Dump:\n\n";
+# Ascertain our current directory.
+my $currentDir = getcwd();
 
-foreach $key (split(/; /, $query->raw_cookie())) {
-    print "[$key]\n";
-}
+# Append the current directory to the output.
+$output .= "\nCurrent directory = [$currentDir]\n";
 
-print "\n\n";
+# Append the contents of the environment to the output.
+$output .= "\nEnvironment Dump:\n\n";
+foreach $key (sort keys %ENV) {
+    $output .= "[$key] = [$ENV{$key}]\n";
+} 
 
-print "</pre></body></html>\n";
+# Append any CGI parameters to the output.
+
+my $cgi = new CGI;
+
+$output .= "\nCGI Parameter Dump:\n\n";
+ 
+foreach $key ($cgi->param()) {
+    $output .= "[$key] = [" . $cgi->param($key) . "]\n";
+} 
+
+# Mark the end of the story.
+$output .= "\nEnd of Output.\n";
+
+# Show our output to the browser.
+print $cgi->header('text/plain'), $output;
+
